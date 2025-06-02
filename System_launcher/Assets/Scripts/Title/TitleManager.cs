c180ea2f74674383d2db1008486ed8b635a07efc
JeongKyunHwi <rbsgnl123@gmail.com>
2025-06-02 17:35:35 +0900

    Inventory

--- System_launcher/Assets/Scripts/Title/TitleManager.cs
+++ System_launcher/Assets/Scripts/Title/TitleManager.cs
@@ -48,6 +48,8 @@ public class TitleManager : MonoBehaviour
 
         AudioManager.Instance.OnLoadUserData();
 
+        UIManager.Instance.EnableStatsUI(false);
+
         StartCoroutine(LoadGameCo());
     }
 
